2613ea34a01cecd82d2ad22ef741e1a0b6e9f91d
YuYang Wang <zenray0780@yahoo.com>
2023-10-13 13:01:31 -0700

    Fixed function.

--- myjs.js
+++ myjs.js
@@ -1,15 +1,17 @@
-alert('Welcome to the shop!')
-
-let securityAnswer = prompt('On average how many months do potatoes take to mature (Answer number 0-5 please.)')
-while(securityAnswer != 4){
-    securityAnswer = prompt('Wrong! Please answer again.')
+function welcome (){
+    alert('Welcome to the shop!');
+    let securityAnswer = prompt('On average how many months do potatoes take to mature (Answer number 0-5 please.)');
+    while(securityAnswer != 4){
+        securityAnswer = prompt('Wrong! Please answer again.');
+    }
+    alert('Congrats! You answer right!');
 }
 
 function ageLimit (){
     const userAnswer1 = prompt('Are you 18+ years old? (Fill only number please.)');
     console.log('User typed:' + userAnswer1);
     if(userAnswer1 >= 18){
-        const userAnswer2 = prompt('Adult content warning! Are you sure you are 18+? (Fill only yes or no please.)').toLowerCase();
+        const userAnswer2 = prompt('Are you sure you are 18+? (Fill only yes or no please.)').toLowerCase();
         if(userAnswer2 === "yes"){
             document.write('Come on in!');
         } else {
@@ -18,7 +20,7 @@ function ageLimit (){
     } else {
         document.write('You are too young for this! Come back later.');
     }
-    return userAnswer1;
+    document.write(' You claim your age is ' + userAnswer1 + ' years old.');
 }
-const userAge = ageLimit();
-document.write(' You claim your age is ' + userAge + ' years old.');
+
+
